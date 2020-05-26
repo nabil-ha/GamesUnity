6d13dab5cea054e76af77f4a87262e70a0479a10
Nabil H <48418186+nabil25x@users.noreply.github.com>
2020-05-26 17:35:51 +0300

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -6,8 +6,6 @@ public class PlayerMovment : MonoBehaviour
 {
     public CharacterController controller;
     public Transform player;
-    public Vector3 firstgame;
-    public Vector3 door_position1;
     public float speed = 12.0f;
    
      
@@ -22,10 +20,6 @@ public class PlayerMovment : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
-        if (Vector3.Distance((player.position), door_position1) < range)
-        {
-            transform.position = firstgame;
-        }
     }
 
 
